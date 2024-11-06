69df0d3b5366694a279a33848dbcf19d3f75288b
Laury Baillon <laury-baillon@hotmail.fr>
2024-11-06 10:33:58 +0100

    mise en place fetch cart.js

--- cart.js
+++ cart.js
@@ -1,33 +1,46 @@
-const { all } = require("../backend/routes");
+// import moment from 'moment';
+// console.log(moment.now());
 
 function updateDeleteTravelEventListener() {
-	for (let i = 0; i < document.querySelectorAll('.deleteTravel').length; i++) {
-		document.querySelectorAll('.deleteTravel')[i].addEventListener('click', function () {
-			fetch(`http://localhost:3000/travel/${this.id}`, { method: 'DELETE' })
-				.then(response => response.json())
-				.then(data => {
-					if (data.result) {
-						this.parentNode.remove();
-					}
-				});
-		});
-	}
+  for (let i = 0; i < document.querySelectorAll(".deleteTravel").length; i++) {
+    document
+      .querySelectorAll(".deleteTravel")
+      [i].addEventListener("click", function () {
+        fetch(`http://localhost:3000/travel/cartFalse/:${this.id}`, { method: "PUT" })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.result) {
+              this.parentNode.remove();
+            }
+          });
+      });
+  }
 }
 
-//departure time à modifier !!!
-fetch('http://localhost:3000/travel')
-.then(response => response.json())
-.then(allTravels => {
-    const travelsInCart = allTravels.filter((travel) => travel.cart === true)
-    for (item of travelsInCart) {
-        document.querySelector('#cart').innerHTML += `
-                <div class="travelDetail">
-                    <div id="path">${item.departure}>${item.arrival}</div>
-                    <div id="departureTime">20:09</div> 
-                    <div id="price">${item.price}</div>
-                    
-                    <button class="deleteTravel">X</button>
-                    
-                </div>`
-    }
-})
+
+fetch("http://localhost:3000/travel")
+  .then((response) => response.json())
+  .then((allTravels) => {
+	console.log(allTravels)
+    const travelsInCart = allTravels.travels.filter((travel) => travel.cart === true);
+	console.log(travelsInCart)
+	if (travelsInCart){
+		for (item of travelsInCart) {
+			let travelDate= new Date(item.date)
+		  document.querySelector("#cartContent").innerHTML += `
+					<div class="travelDetail">
+						<div id="path">${item.departure}>${item.arrival}</div>
+						<div id="departureTime">${travelDate.getHours()}:${travelDate.getMinutes()}</div> 
+						<div id="price">${item.price}€</div>
+						
+						<button class="deleteTravel">X</button>
+						
+					</div>`;
+		} updateDeleteTravelEventListener();
+	}else{
+		document.querySelector("#cartContent").innerHTML += `
+		<p>No tickets in your cart.</p>
+		<p>Why not plan a trip?</p>`
+	}
+    
+  });
